5dcf9c7b9ffe3b076ee77ac6705cedd282844709
Shayla Neitzel <neitzes@wwu.edu>
2025-07-13 00:23:41 -0700

    Implemented main program

--- app/src/main/java/avl/Unique.java
+++ app/src/main/java/avl/Unique.java
@@ -9,63 +9,58 @@ public class Unique {
     /** Main program: prints the number of unique lines in a given file by one
      * of two methods */
     public static void main(String[] args) {
-    //     if (args.length != 2) {
-    //       System.out.println("Requires 2 arguments: naive or avl and a filename.");
-    //       return;
-    //     }
-    //     try {
-    //         File f = new File(args[1]);
-    //         Scanner sc = new Scanner(f);
-    //         System.out.println("Finding unique lines in " + args[1]);
-    //         if (args[0].equals("naive")) {
-    //           System.out.println("Naive:");
-    //           System.out.println(naiveUnique(sc));
-    //         } else {
-    //           System.out.println(args[1]);
-    //           System.out.println("AVL:");
-    //           System.out.println(avlUnique(sc));
-    //         }
-    //     } catch (FileNotFoundException exc) {
-    //         System.out.println("Could not find file " + args[1]);
-    //     }
-    // }
+        if (args.length != 2) {
+          System.out.println("Requires 2 arguments: naive or avl and a filename.");
+          return;
+        }
+        try {
+            File f = new File(args[1]);
+            Scanner sc = new Scanner(f);
+            System.out.println("Finding unique lines in " + args[1]);
+            if (args[0].equals("naive")) {
+              System.out.println("Naive:");
+              System.out.println(naiveUnique(sc));
+            } else {
+              System.out.println(args[1]);
+              System.out.println("AVL:");
+              System.out.println(avlUnique(sc));
+            }
+        } catch (FileNotFoundException exc) {
+            System.out.println("Could not find file " + args[1]);
+        }
+    }
 
-    // /** Return the number of unique lines availble to be read by sc */ 
-    // private static int naiveUnique(Scanner sc) {
-    //   // unique lines seen so far
-    //   ArrayList<String> seen = new ArrayList<String>();
-    //   while (sc.hasNextLine()) {
-    //     String line = sc.nextLine();
+    /** Return the number of unique lines availble to be read by sc */ 
+    private static int naiveUnique(Scanner sc) {
+      // unique lines seen so far
+      ArrayList<String> seen = new ArrayList<String>();
+      while (sc.hasNextLine()) {
+        String line = sc.nextLine();
 
-    //     // check if we've seen it:
-    //     int i = 0;
-    //     while (i < seen.size() && !line.equals(seen.get(i))) {
-    //       i++;
-    //     }
-    //     if (i == seen.size()) {
-    //       seen.add(line);
-    //     }
-    //   }
-    //   return seen.size();
+        // check if we've seen it:
+        int i = 0;
+        while (i < seen.size() && !line.equals(seen.get(i))) {
+          i++;
+        }
+        if (i == seen.size()) {
+          seen.add(line);
+        }
+      }
+      return seen.size();
 
-
-    AVL a = new AVL();
-    a.root = a.new Node("x");
-    a.root.right = a.new Node("y");
-    a.root.right.parent = a.root;
-
-    // check(a);
-    // a.printTree();
-    a.leftRotate(a.root);
-    a.printTree();
 
     }
 
-    // /** Return the number of unique lines availble to be read by sc */ 
-    // private static int avlUnique(Scanner sc) {
-    //   // TODO - implement this; its runtime should be O(n log n)
-    //   return 0; // placeholder, so code compiles
-    // }
+    /** Return the number of unique lines availble to be read by sc */ 
+    private static int avlUnique(Scanner sc) {
+
+        AVL a = new AVL();
+        while (sc.hasNextLine()){
+            a.avlInsert(sc.nextLine());
+        }
+      // TODO - implement this; its runtime should be O(n log n)
+      return a.getSize(); // placeholder, so code compiles
+    }
 
       
 }
